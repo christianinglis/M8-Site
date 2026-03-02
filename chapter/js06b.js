a4a6947f0d338cfadb6ef7c59416dd382eb6e012
Christian Inglis <ccinglis@my.waketech.edu>
2026-03-02 14:00:54 -0500

    Done

--- chapter/js06b.js
+++ chapter/js06b.js
@@ -9,6 +9,10 @@
 
       Filename: js06b.js
  */
+let subButton = document.getElementById("subButton");
+
+subButton.addEventListener("click", validateName);
+
 function validateName() {
    let cardName = document.getElementById("cardName");
    if (cardName.validity.valueMissing) {
@@ -18,8 +22,59 @@ function validateName() {
    }
 }
 
+function validateCard() {
+   let card = document.forms.payment.elements.credit[0];
+   if (card.validity.valueMissing) {
+      card.setCustomValidity("Select your credit card");
+   } else {
+      card.setCustomValidity("");
+   }
+}
 
+function validateNumber() {
+      let cNum = document.getElementById("cardNumber");
+      if (cNum.validity.valueMissing) {
+         cNum.setCustomValidity("Enter a valid card number");
+   } else if (cNum.validity.paternMismatch) {
+      cNum.setCustomValidity("Enter a valid card number");
+   } else if (luhn(cNum.value) === false) {
+      cNum.setCustomValidity("Enter a legitimate card number");
+   } else {
+      cNum.setCustomValidty("");
+   }
+}
+function validateMonth() {
+   let month = document.getElementById("expMonth");
+   if (month.selectedIndex === 0) {
+      month.setCustomValidity("Select the expiration month");
+   } else {
+      month.setCustomValidity("");
+   }
+}
 
+function validateYear() {
+   let year = document.getElementById("expYear");
+   if (year.selectedIndex === 0) {
+      year.setCustomValidity("Select the expiration year");
+   } else {
+      year.setCustomValidity("");
+   }
+}
+
+function validateCVC() {
+   let card = document.querySelector('input[name="credit"]:checked').value;
+   let cvc = document.getElementById("cvc");
+
+   if (cvc.validity.valueMissing) {
+      cvc.setCustomValidity("Enter your CVC number");
+   } else if ((card === "amex") && !(/^\d{4}$/.test(cvc.value))) {
+      cvc.setCustomValidity("Enter a 4-digit number");
+   } else if ((card !== "amex") && !(/^\d{3}$/.test(cvc.value))) {
+      cvc.setCustomValidity("Enter a 3-digit number");
+   } else {
+      cvc.setCustomValidity("");
+   }
+}
 
 
 
